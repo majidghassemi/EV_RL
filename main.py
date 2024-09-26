e6a939cd7685656a71ae0fb15c7411d6342251f3
majidghassemi <mjghcr@gmail.com>
2024-09-26 11:29:10 -0400

    Updated main file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from road_network import RandomGraph
 from TERC2 import TERC2
 from HQLearning import QLearning
+from SimpleQLearning import SimpleQLearning
 from DQN import DQNetwork, DQNAgent
 import networkx as nx
 import pickle
@@ -9,61 +10,90 @@ import random
 import matplotlib.pyplot as plt
 
 # Generate the road network using RandomGraph
-# graph_generator = RandomGraph(45, 0.25, 42, 3)
-# generated_graph, charging_stations = graph_generator.generate_graph(show_stat=True)
+graph_generator = RandomGraph(100, 0.085, 42, 1)
+generated_graph, charging_stations = graph_generator.generate_graph(show_stat=True)
 
-# # Initialize TERC2 with the generated graph
-# terc2 = TERC2(generated_graph)
+# Initialize TERC2 with the generated graph
+terc2 = TERC2(generated_graph)
 
-# # Add charging nodes to TERC2
-# for station in charging_stations:
-#     terc2.add_charging_nodes(station)
+# Add charging nodes to TERC2
+for station in charging_stations:
+    terc2.add_charging_nodes(station)
 
-# # Export results for Q-learning initialization
-# terc2.export_terc_results("terc_results.pkl")
-# terc2.export_to_qlearning("q_values.pkl")
+# Export results for Q-learning initialization
+terc2.export_terc_results("terc_results.pkl")
+terc2.export_to_qlearning("q_values.pkl")
 
-# # Extract the adjacency matrix from the generated graph
-# adjacency_matrix = nx.to_numpy_array(generated_graph)
+# Extract the adjacency matrix from the generated graph
+adjacency_matrix = nx.to_numpy_array(generated_graph)
 
-# # Set start and end state
-# start_state = 93
-# end_state = 15
+# Set start and end state
+start_state = 67
+end_state = 2
 
-# # Initialize Q-learning with the adjacency matrix and charging stations
-# # q_learning = QLearning(adjacency_matrix=adjacency_matrix, 
-# #                        num_nodes=len(generated_graph.nodes), 
-# #                        charging_stations=charging_stations,  # Pass charging stations
-# #                        q_values_file="q_values.pkl", 
-# #                        epsilon=0.2,  # Initial epsilon for exploration
-# #                        alpha=0.1,  # Learning rate
-# #                        epsilon_decay_rate=0.995)  # Decay rate for epsilon
+# HQLEARNING TESTING
 
-# # Run Q-learning using start_state and end_state
-# best_path, best_reward = q_learning.q_learning(start_state=start_state, end_state=end_state, num_epoch=250, visualize=True, save_video=True)
+# Initialize Q-learning with the adjacency matrix and charging stations
+q_learning = QLearning(adjacency_matrix=adjacency_matrix, 
+                       num_nodes=len(generated_graph.nodes), 
+                       charging_stations=charging_stations,  # Pass charging stations
+                       q_values_file="q_values.pkl")  # Decay rate for epsilon
 
-# # Output the results
-# print(f"Best path found by DQN: {best_path}")
-# print(f"Best reward achieved: {best_reward}")
+# Run Q-learning using start_state and end_state
+best_path, best_reward, best_travel_time, *_ = q_learning.q_learning(start_state=start_state, end_state=end_state, num_epoch=250, visualize=True, save_video=True)
 
-# # Plot Q-value convergence and save it
-# plt.plot(q_learning.q_convergence)
-# plt.xlabel('Epoch')
-# plt.ylabel('Max Q-Value Change')
-# plt.title('Q-Value Convergence')
-# plt.savefig("q_value_convergence.png")
-# plt.close()
+# Output the results
+print(f"Best path found by TQ Learning: {best_path}")
+print(f"Best reward achieved: {best_reward}")
+print(f"Minimized travel time: {best_travel_time}")
 
-# # Plot reward per epoch and save it
-# plt.figure()
-# plt.plot(q_learning.epoch_rewards)
-# plt.xlabel('Epoch')
-# plt.ylabel('Total Reward')
-# plt.title('Reward per Epoch')
-# plt.savefig("reward_per_epoch.png")
-# plt.close()
+# Plot Q-value convergence and save it
+plt.plot(q_learning.q_convergence)
+plt.xlabel('Epoch')
+plt.ylabel('Max Q-Value Change')
+plt.title('Q-Value Convergence for TQ learning')
+plt.savefig("q_value_convergence_HQ.png")
+plt.close()
 
+# Plot reward per epoch and save it
+plt.figure()
+plt.plot(q_learning.epoch_rewards)
+plt.xlabel('Epoch')
+plt.ylabel('Total Reward')
+plt.title('Reward per Epoch for TQ Learning')
+plt.savefig("reward_per_epoch_HQ.png")
+plt.close()
 
+# Q Learning TESTING
+
+sq_learning = SimpleQLearning(adjacency_matrix=adjacency_matrix, 
+                       num_nodes=len(generated_graph.nodes), 
+                       charging_stations=charging_stations)  # Decay rate for epsilon
+
+# Run Q-learning using start_state and end_state
+best_path_q, best_reward_q, best_travel_time_q, *_ = sq_learning.sq_learning(start_state=start_state, end_state=end_state, num_epoch=250, visualize=True, save_video=True)
+
+# Output the results
+print(f"Best path found by Q Learning: {best_path_q}")
+print(f"Best reward achieved: {best_reward_q}")
+print(f"Minimized travel time: {best_travel_time_q}")
+
+# Plot Q-value convergence and save it
+plt.plot(sq_learning.q_convergence)
+plt.xlabel('Epoch')
+plt.ylabel('Max Q-Value Change')
+plt.title('Q-Value Convergence for Q learning')
+plt.savefig("q_value_convergence.png")
+plt.close()
+
+# Plot reward per epoch and save it
+plt.figure()
+plt.plot(sq_learning.epoch_rewards)
+plt.xlabel('Epoch')
+plt.ylabel('Total Reward')
+plt.title('Reward per Epoch for Q learning')
+plt.savefig("reward_per_epoch.png")
+plt.close()
 
 # DQN RESULTS TESTING
 
